f090f549c8770673b2a5fb676e457c0fb4841a2a
staygood-bekind <staygoodbekind@gmail.com>
2025-09-11 01:42:10 +0300

    Create lyrics.js

---
+++ lyrics.js
@@ -0,0 +1,12 @@
+// lyrics.js
+const lyricsData = {
+  song1: `Imagine all the people living life in peace...
+Imagine there's no heaven...
+Imagine all the people living life as one...`,
+
+  song2: `Is this the real life? Is this just fantasy?
+Caught in a landslide, no escape from reality
+Open your eyes, look up to the skies and see...`
+  
+  // Add more songs as needed
+};
