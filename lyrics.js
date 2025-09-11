c6ca3fd2676ca56d06c9a49ebdac845e422f1ae1
staygood-bekind <staygoodbekind@gmail.com>
2025-09-11 22:37:44 +0300

    Update lyrics.js

--- lyrics.js
+++ lyrics.js
@@ -48,9 +48,81 @@ const lyricsData = {
    ሞተይ እንተ ኣውረየ ትምኒትዩ ጸላእየይ
    ኣነ አለኹ’የ ካብ ዘይጠፍእ ዘርኢ’የ`,
   //-------------------------------------------------------------
-  song2: `Is this the real life? Is this just fantasy?
+    // Author:
+  // Album:
+  // Genre:
+  // Year:
+  song4: `​1) ኣየሕፍረንን ትህላውነትካ 
+     እኳ ደኣ ይብለኒ ተንስእ ካብ ምውዳቅካ
+     ሰላመይ ይሰፍን ምሳኻ ክከውን
+     እቲ ባዶ ልበይ ብርሃንካ ይውንን
+
+2) ነቲ ቅኑዕ ዓወት ከምትህቦ
+     የሱሰይ ንንጹህ ካብ ክፉእ ትሕልዎ
+     ኣነ ግና ወልሓንቲ  ዘይብለይ 
+     ኩሉ ረኺበዮ ብፍቅርኻ ጎይታይ
+
+3) ባሕሪ መቀሉ ህዝቡኳ ዘሕለፈ 
+      ናተይ ቀላያት እንታይ ከይገብሮ'ለ
+      ምስ ለመንክዎ ኣብ ኣፍ ቤትካ ኮይነ 
+      ዝረኣኽዎ ግብሪ ምግላጹ ስኢነ        (2)
+
+ካብ ልበይ ምስጋና ናባኻ ይድርጉሖ 
+ነቲ ዝረእኽዎ (ነዚ ዘለኽዎ) ገለ እንተገለጾ 
+
+ እንታይ ኣሎ እሞ ኣነ ዝህበካ
+ብዘይካ ምምስጋን ምግላስ ውዕለትካ
+እንታይ ኣሎ እሞ ኣነ ዝህበካ
+የሱሰይ ምምስጋን'ዩ ምግላስ ውዕለትካ
+
+መዛ፦ ብግርኻ (ብተኣምራትካ) ተደኒቀ ከመስግነካ ፈቲነ
+    ኣበይ አሞ ኣይ ኣኸሎን እቲ ቃላት ንውዕለትካ (ንግብርኻ) (2)
+           መግለጺ ሓጎሰይ ግና ክበር ንገስ'የ ዝብለካ
+
+ልዑል ኣምላኽ ፍግም ኢለ እሰግደልካ
+ብሓጎስ ደስታ ተመሊአ ይዕልለልካ
+ዕቤትካ ሪኤ የሱሰይ እሽብሽበልካ
+ገለ ተገለጽኩ ካብ ውዕለትካ እቃንየልካ
+ብተኣምራትካ ተደኒቀ አመስግነካ
+               ብግርኻ......,
+
+  "song4.1": 1) ayeḥfärenen tiḥlawnetika
+ekwa de’a yibläni tenesi’ kab miwuda’ika
+selamey yisefen misaka kikewn
+eti bado libey birhanka yiwunən
+2) neti qenu’ awet kemtiḥbo
+Yesusey nints’uḥ kab kefu’ tiḥlowo
+ane gna welḥanti zeybley
+kulu rəḥibeyo bifqerka goytay
+3) baḥri meqelu ḥzbuqua zeḥläfe
+natey qelayat entay keygebero’lä
+mis lemenkewo ab af beytka koyne
+zire’aḥwo gəbri miglats’u si’ine (2)
+
+kab libey misgana nabaḥa yidriguho
+neti zire’əkwo (nezi zelekwo) gele entegela’tso
+
+entay alo emo ane zeḥbeka
+bizäyka mimsgan miglas wu’letka
+entay alo emo ane zeḥbeka
+Yesusey mimsgan’yu miglas wu’letka
+
+bigərka (bita’emratika) tedeniqe amesgineka fətine
+abey amo ay akhelon eti qalat niwu’letka (nigbrika) (2)
+megəlätsi ḥagosay gna kiber niges’eye zibleka
+
+li’ul Amlak fgm ile esegidelka
+biḥagos desta temelia yi’əllelka
+’beytka ri’e Yesusey eshibshebelka
+gele tegeläts’ku kab wu’letka eqanyelka
+bita’emratika tedeniqe amesgineka
+bigərka……,
+  //-------------------------------------------------------------
+  song2: Is this the real life? Is this just fantasy?
 Caught in a landslide, no escape from reality
-Open your eyes, look up to the skies and see...`
-  
+Open your eyes, look up to the skies and see...
+
+
+`
   // Add more songs as needed
 };
